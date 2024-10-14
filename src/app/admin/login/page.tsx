f12fd3329d1eb27f477ee939498f0f89b7d2afe5
Fn-front <reservoir-station@outlook.jp>
2024-10-14 14:31:14 +0900

    add: pages

---
+++ src/app/admin/login/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminLogin() {
+  return (
+    <>
+      <p>adminログインページ</p>
+    </>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <>
       <h2 className='c_h2'>テンプレ</h2>
+      <a href='/admin/'>admin</a>
     </>
   );
 }
