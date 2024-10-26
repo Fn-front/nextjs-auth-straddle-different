721c290f9163c539aeb2978beca930e23a8e335e
Fn-front <reservoir-station@outlook.jp>
2024-10-26 10:59:36 +0900

    add: page link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ export default function Home() {
   return (
     <>
       <h2 className='c_h2'>ディレクトリ別ログイン認証</h2>
-      <a href='/admin/'>URL：admin</a>
+      <a href='/admin/'>URL：admin</a><br />
+      <a href='/user/'>URL：user</a>
     </>
   );
 }
